4770314138fb22bc236c33bd83ccf8ad6e93b65a
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-08-01 16:19:44 -0500

    changed sqare for box2 to rectangle

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -4,7 +4,7 @@ public class ShapesTest {
 
     public static void main(String[] args) {
         Rectangle box1= new Rectangle(4,5);
-        Square box2= new Square(5);
+        Rectangle box2= new Square(5);
 
         System.out.println(box1.getPerimeter());
         System.out.println(box1.getArea());
