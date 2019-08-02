24873ac8ab75d9787af6a733c7432dd41cb426e7
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-08-01 22:04:32 -0500

    movies exercise

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,64 +1,83 @@
 package movies;
 import util.Input;
+import static movies.MoviesArray.findAll;
+//why do we import this movies.MovieArray.findall(); this way?
+
+
 public class MoviesApplication {
+
+//    public static Movie[] displayMovies(Movie []movie) {
+//        for (int i = 0; i < movie.length; i += 1) {
+//            System.out.println(movie[i]);
+//
+//        }
+//        return movie;
+//
+//    }
 
 
     public static void main(String[] args) {
-       Input input = new Input();
+        //        how do i import the findall() method in the MoviesArray?
 
 
-       Movie[] movie=MoviesArray.findAll();
 
+        Input input = new Input();
 
-       String whatMovie="What would you like to do?\n";
-       whatMovie +="0 - exit\n" +
-               "\n1 - view all movies\n" +
-               "2 - view movies in the animated category\n" +
-               "3 - view movies in the drama category\n" +
-               "4 - view movies in the horror category\n" +
-               "5 - view movies in the scifi category\n";
-      whatMovie+="\nEnter your choice:\n";
+        String  whatMovie = "What would you like to do?\n";
+                whatMovie += "0 - exit\n";
+                whatMovie += "1 - view all movies\n";
+                whatMovie += "2 - view movies in the animated category\n";
+                whatMovie += "3 - view movies in the drama category\n" ;
+                whatMovie += "4 - view movies in the horror category\n";
+                whatMovie += "5 - view movies in the scifi category\n";
+                whatMovie += "\nEnter your choice:\n";
 
         System.out.println(whatMovie);
-        int userInput= input.getInt(0,5);
+        int userInput = input.getInt(0, 5);
 
-        switch(userInput) {
-            case 0:
-                // code block
-                break;
+
+        switch (userInput) {
             case 1:
-                // code block
+                for (Movie movie : findAll()) {
+                    System.out.println(movie.getName() + "----" + movie.getCategory());
+                }
                 break;
             case 2:
-                // code block
+                for (Movie movie : findAll()) {
+                    if(movie.getCategory().equals("animated")){
+                    System.out.println(movie.getName() + "----" + movie.getCategory());
+                }
+                }
                 break;
             case 3:
-                // code block
+                for(Movie movie : findAll()) {
+                    if(movie.getCategory().equals("drama")){
+                    System.out.println(movie.getName() + "----" + movie.getCategory());
+                    }
+                }
+
                 break;
             case 4:
-                // code block
+                for(Movie movie : findAll()) {
+                    if(movie.getCategory().equals("horror")){
+                    System.out.println(movie.getName() + "----" + movie.getCategory());
+                    }
+                }
                 break;
             case 5:
-                // code block
+                for(Movie movie : findAll()) {
+                    if(movie.getCategory().equals("scifi")){
+                    System.out.println(movie.getName() + "----" + movie.getCategory());
+                    }
+                }
                 break;
-
-
-
-
             default:
-                // code block
+                System.out.println("cool input");
+
+                break;
         }
-
-
-
-        }
-//        int answer=input.getInt(0,5);
-
-
-
-
-
-
 
     }
+
+    } //class end bracket//
 
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,23 +1,21 @@
 package shapes;
 
 public class Square extends Rectangle{
-    protected long side;
 
 //    should define a constructor that accepts one argument, side, and calls the parent's constructor to set both the length and width to the value of side.
     public  Square(int side){
 
         super(side,side);
-        this.side=side;
 
     }
 
 //    override getarea getperimeter
 public  long getArea(){
-    return this.side * this.side;
+    return this.width * this.length;
 
 }
     public long getPerimeter(){
-        return this.side * 4;
+        return this.width * 4;
 
 
     }
