3b6043def5c46995f13df722987de240c5a57065
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-08-04 22:01:01 -0500

    exercise for student grade average

---
+++ src/grades/Student.java
@@ -0,0 +1,46 @@
+package grades;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Collections;
+
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+
+
+//constructor
+
+    public Student(String name) {
+        this.name=name;
+        this.grades = new ArrayList<>();
+    }
+
+//methods
+
+
+// returns the student's name
+    public String getName(){
+        return this.name;
+    }
+// adds the given grade to the grades property
+    public void addGrade(int grade){
+        this.grades.add(grade);
+    }
+
+// returns the average of the students grades
+
+    public double getGradeAverage(){
+        Collections.sort(grades);
+        int sum = 0;
+        for(int i=0; i < grades.size() ; i++)
+            sum +=grades.get(i);
+        //calculate average value
+        double average = sum / grades.size();
+        return average;
+
+
+    }
+
+
+}
---
+++ src/grades/StudentTest.java
@@ -0,0 +1,27 @@
+package grades;
+
+public class StudentTest {
+
+    public static void main(String [] args){
+
+         Student newStudent= new  Student("Alex");
+         newStudent.addGrade(80);
+         newStudent.addGrade(95);
+         newStudent.addGrade(100);
+
+        Student newStudent2= new Student("Efrain");
+        newStudent2.addGrade(75);
+        newStudent2.addGrade(85);
+        newStudent2.addGrade(95);
+
+
+
+
+        System.out.println(newStudent.getGradeAverage());
+        System.out.println(newStudent2.getGradeAverage());
+
+
+
+
+    }
+}
